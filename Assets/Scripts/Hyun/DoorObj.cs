2cf0041fbf3df95ecf84b25a9f616f21ae83bd0a
jungwook123 <24sunrin119@sunrint.hs.kr>
2025-07-19 03:04:45 +0900

    commit

--- Assets/Scripts/Hyun/DoorObj.cs
+++ Assets/Scripts/Hyun/DoorObj.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class DoorObj : Object
 {
     public string sceneName;
+    public GameState gameState;
     public override void Interact()
     {
         base.Interact();
         SceneTransitionLight.Instance.TransitionToScene(sceneName);
+        GameManager.Instance.gameState = gameState;
     }
 }
--- Assets/Scripts/Hyun/Portal.cs
+++ Assets/Scripts/Hyun/Portal.cs
@@ -5,6 +5,7 @@ public class Portal : Object
     public override void Interact()
     {
         base.Interact();
+        if(GameManager.Instance.gameState != GameState.Onrunning) return;
         SceneTransitionLight.Instance.TransitionToScene("Home");
     }
 }
