5a16a299b854792954266e9045292e9f6a10b889
jungwook123 <24sunrin119@sunrint.hs.kr>
2025-07-18 18:23:49 +0900

    111

--- Assets/Scripts/Management/GameManager.cs
+++ Assets/Scripts/Management/GameManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
 
+public enum GameState
+{
+    Ongoing,// 은행가는중
+    InBank,// 은행안
+    Onrunning // 도망치는중
+}
 public class GameManager : Singleton<GameManager>
 {
     public int playerMoney =500;
-    
+    public GameState gameState = GameState.Onrunning;
     public override void Awake()
     {
         base.Awake();
